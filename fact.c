2732775f1e327ce9f7caae613623940c73e941e2
Nareshrcr <82564394+Nareshrcr@users.noreply.github.com>
2021-09-19 15:59:21 +0530

    fact.c
    factorial file is added

---
+++ fact.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+reverse ()
+{
+  char str[1000], rev[1000];
+  int i, j, count = 0;
+  scanf("%s", str);
+  printf("\nString Before Reverse: %s", str);
+  //finding the length of the string
+  while (str[count] != '\0')
+  {
+    count++;
+  }
+  j = count - 1;
+
+  //reversing the string by swapping
+  for (i = 0; i < count; i++)
+  {
+    rev[i] = str[j];
+    j--;
+  }
+
+  printf("\nString After Reverse: %s", rev);
+}
+[ec2-user@ip-172-31-45-22 test-c-source]$ cat fact.c
+#include<stdio.h>
+factorial ()
+{
+ int i,fact=1,number;
+ printf("Enter a number: ");
+  scanf("%d",&number);
+    for(i=1;i<=number;i++){
+      fact=fact*i;
+  }
+  printf("Factorial of %d is: %d",number,fact);
+return 0;
+}
